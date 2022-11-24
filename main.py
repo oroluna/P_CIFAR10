29a2df4b1671e56aaeef8bf312c8450071ab16d3
jorge-armando-navarro-flores <jorge.navarro1817@alumnos.udg.mx>
2022-11-24 11:31:21 -0600

    start

--- main.py
+++ main.py
@@ -39,7 +39,9 @@ def my_app(cfg: DictConfig) -> None:
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
+
     # Trainset
+
     trainset = torchvision.datasets.CIFAR10(
         root=cfg.paths.data, train=True, download=True, transform=transform_train)
     # Sample train subset
@@ -59,17 +61,30 @@ def my_app(cfg: DictConfig) -> None:
     testloader = torch.utils.data.DataLoader(
         sample_test, batch_size=cfg.params.test.batch_size, shuffle=False, num_workers=2)
 
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+
     # Model
     print('==> Building model..')
-
-    # instantiante model
+    # net = VGG('VGG19')
+    # net = ResNet18()
+    # net = PreActResNet18()
+    # net = GoogLeNet()
+    # net = DenseNet121()
+    # net = ResNeXt29_2x64d()
+    # net = MobileNet()
+    # net = MobileNetV2()
+    # net = DPN92()
+    # net = ShuffleNetG2()
+    # net = SENet18()
+    # net = ShuffleNetV2(1)
+    # net = EfficientNetB0()
+    # net = RegNetX_200MF()
     net = hydra.utils.instantiate(cfg.model)
     net = net.to(device)
     if device == 'cuda':
         net = torch.nn.DataParallel(net)
         cudnn.benchmark = True
-
-    # If True resume The last training and restart from the last epoch
 
     if cfg.args.resume:
         # Load checkpoint.
@@ -81,18 +96,11 @@ def my_app(cfg: DictConfig) -> None:
         start_epoch = checkpoint['epoch']
 
     criterion = nn.CrossEntropyLoss()
-    print(cfg.args.lr)
-    # optimizer = optim.SGD(net.parameters(), lr=cfg.args.lr,
-    #                       momentum=0.9, weight_decay=cfg.params.lr)
-    print("PARAMS", dict(cfg.optimizer.params))
     optimizer = hydra.utils.call(cfg.optimizer, params=net.parameters(), **cfg.optimizer.params)
-
-
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
     # Training
     def train(epoch):
-        global train_iteration
         print('\nEpoch: %d' % epoch)
         net.train()
         train_loss = 0
@@ -113,35 +121,17 @@ def my_app(cfg: DictConfig) -> None:
 
             progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
-            train_writer.writerow({'acumulated_iteration': train_iteration, 'relative_iteration': batch_idx,
-                                   'epoch': 1 / len(trainloader) * train_iteration, 'accuracy': 100. * correct / total,
-                                   'loss': train_loss / (batch_idx + 1)})
-            train_iteration += 1
 
     def test(epoch, best_acc=best_acc):
-        global test_iteration
-        best_acc
+
         net.eval()
         test_loss = 0
         correct = 0
         total = 0
         with torch.no_grad():
-            y_pred = []
-            y_true = []
-
             for batch_idx, (inputs, targets) in enumerate(testloader):
                 inputs, targets = inputs.to(device), targets.to(device)
                 outputs = net(inputs)
-
-                # Confusion matrix
-                output = (torch.max(torch.exp(outputs), 1)[1]).data.cpu().numpy()
-                y_pred.extend(output)  # Save Prediction
-
-                labels = targets.data.cpu().numpy()
-                y_true.extend(labels)  # Save Truth
-
-                # constant for classes
-
                 loss = criterion(outputs, targets)
 
                 test_loss += loss.item()
@@ -151,22 +141,6 @@ def my_app(cfg: DictConfig) -> None:
 
                 progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                              % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
-
-                test_writer.writerow({'acumulated_iteration': test_iteration, 'relative_iteration': batch_idx,
-                                      'epoch': 1 / len(testloader) * test_iteration, 'accuracy': 100. * correct / total,
-                                      'loss': test_loss / (batch_idx + 1)})
-                test_iteration += 1
-
-        class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-                       'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-
-        cf_matrix = confusion_matrix(y_true, y_pred)
-        make_confusion_matrix(OUTPUT_ROUTE, y_true=y_true,
-                              y_pred=y_pred,
-                              classes=class_names,
-                              figsize=(15, 15),
-                              text_size=10)
 
         # Save checkpoint.
         acc = 100. * correct / total
@@ -182,35 +156,10 @@ def my_app(cfg: DictConfig) -> None:
             torch.save(state, './checkpoint/ckpt.pth')
             best_acc = acc
 
-            # Save metrics in outputs/year-month-day/hour-minut-second
-            hydra_cfg = hydra.core.hydra_config.HydraConfig.get()
-            with open(f"{hydra_cfg['runtime']['output_dir']}/main.log", "w") as file:
-                file.write('Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                           % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
-
-    #### SAVE IN TO CSV ####
-    hydra_cfg = hydra.core.hydra_config.HydraConfig.get()
-    OUTPUT_ROUTE = hydra_cfg['runtime']['output_dir']
-
-
-    with open(f"{OUTPUT_ROUTE}/train.csv", "w") as csv_train, open(f"{OUTPUT_ROUTE}/test.csv", "w") as csv_test:
-        fieldnames = ['acumulated_iteration', 'relative_iteration', 'epoch', 'accuracy', 'loss']
-        train_writer = csv.DictWriter(csv_train, fieldnames=fieldnames)
-        train_writer.writeheader()
-
-        test_writer = csv.DictWriter(csv_test, fieldnames=fieldnames)
-        test_writer.writeheader()
-
-        for epoch in range(start_epoch, start_epoch + cfg.params.epoch_count):
-            train(epoch)
-
-            test(epoch)
-            scheduler.step()
-
-    plot_moving_average(OUTPUT_ROUTE)
-    plot_train_log(OUTPUT_ROUTE)
-    plot_loss(OUTPUT_ROUTE)
-    plot_accuracy(OUTPUT_ROUTE)
+    for epoch in range(start_epoch, start_epoch + cfg.params.epoch_count):
+        train(epoch)
+        test(epoch)
+        scheduler.step()
 
 
 if __name__ == "__main__":
